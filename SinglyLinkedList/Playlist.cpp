8437b743c93088172006cd9c4b9dfeb8b49219ce
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-04-09 22:31:14 +0900

    init: SinglyLinkedList

--- SinglyLinkedList/Playlist.cpp
+++ SinglyLinkedList/Playlist.cpp
@@ -1,3 +1,17 @@
 //
 // Created by 김태경 on 2023/04/05.
 //
+#include "Playlist.h"
+
+void Playlist::addSong(std::string name, std::string artist) {
+
+}
+
+void Playlist::removeSong(Song song) {
+
+}
+
+void Playlist::printPlaylist() {
+
+}
+
--- SinglyLinkedList/SinglyLinkedList.cpp
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -1,3 +1,17 @@
 //
 // Created by 김태경 on 2023/04/05.
 //
+#include "SinglyLinkedList.h"
+
+void SinglyLinkedList::addNode(Song data) {
+
+}
+
+void SinglyLinkedList::removeNode(Song data) {
+
+}
+
+void SinglyLinkedList::printList() {
+
+}
+
