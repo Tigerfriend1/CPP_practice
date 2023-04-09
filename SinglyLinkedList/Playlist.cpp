c7ccb7220720169c8f98ba4a6de10b4d850ead30
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-04-09 23:36:46 +0900

    구현완료: singlyLinkedList-앞에서추가하는 버전과 뒤에 추가하는 버전

--- SinglyLinkedList/Playlist.cpp
+++ SinglyLinkedList/Playlist.cpp
@@ -2,16 +2,21 @@
 // Created by 김태경 on 2023/04/05.
 //
 #include "Playlist.h"
+#include <iostream>
+using namespace std;
 
 void Playlist::addSong(std::string name, std::string artist) {
+    Song *a=new Song(name,artist);
 
+    this->songList->addNode(*a);
 }
 
 void Playlist::removeSong(Song song) {
-
+    this->songList->removeNode(song);
 }
 
 void Playlist::printPlaylist() {
-
+    cout<<"Playlist:"<<endl;
+    this->songList->printList();
 }
 
--- SinglyLinkedList/SinglyLinkedList.cpp
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -2,16 +2,48 @@
 // Created by 김태경 on 2023/04/05.
 //
 #include "SinglyLinkedList.h"
+#include <iostream>
+using namespace std;
 
 void SinglyLinkedList::addNode(Song data) {
-
+    Node *New=new Node(data);
+    if (this->head== nullptr){
+        head=New;
+    }
+    else {
+        Node *current = head;
+        New->next = current;
+        head=New;
+    }
+//    if (this->head== nullptr){
+//        this->head=New;
+//    }
+//    else {
+//        Node *current=this->head;
+//        while(current->next!= nullptr){
+//            current=current->next;
+//        }
+//        current->next=New;
+//    }
 }
 
 void SinglyLinkedList::removeNode(Song data) {
-
+    Node *current=this->head;
+    Node *pre= nullptr;
+    while(current!= nullptr && !current->data.equals(data)){
+        pre=current;
+        current=current->next;
+    }
+    pre->next=current->next;
+    delete current;
 }
 
 void SinglyLinkedList::printList() {
-
+    Node *current=this->head;
+    while(current!= nullptr){
+        cout<<current->data.toString()<<endl;
+        current=current->next;
+    }
+    cout<<endl;
 }
 
