6d4ff09adf323aed9923d0b0dc7d5aeac8a0a224
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-04-19 11:44:06 +0900

    refactor: Cell객체를 생성하지 않고 수행하게하여 overhead줄임

--- Excel_Cell/Excel.cpp
+++ Excel_Cell/Excel.cpp
@@ -7,24 +7,36 @@
 using namespace std;
 
 double Excel::average(int fromRow, int fromCol, int toRow, int toCol) const {
-    double val=0;
+//    double val=0;
+//    for (int i=0; i<(toRow-fromRow+1); i++){
+//        Cell a=this->data[fromRow+i][fromCol];
+//        if (a.getType()==INT){ //최대한 private보다는 함수호출로 값에 접근
+//            val+=a.getIntVal();
+//        }
+//        else if(a.getType()==DOUBLE){
+//            val+=a.getDoubleVal();
+//        }
+//        else{
+//            continue;
+//        }
+//    }
+//    val=val/(toRow-fromRow+1);
+//    return val;
+    double val =0;
     for (int i=0; i<(toRow-fromRow+1); i++){
-        Cell a=this->data[fromRow+i][fromCol];
-        if (a.getType()==INT){ //최대한 private보다는 함수호출로 값에 접근
-            val+=a.getIntVal();
+        if(data[fromRow+i][fromCol].getType()==INT){
+            val+=data[fromRow+i][fromCol].getIntVal();
         }
-        else if(a.getType()==DOUBLE){
-            val+=a.getDoubleVal();
+        else if(data[fromRow+1][fromCol].getType()==DOUBLE){
+            val+=data[fromRow+i][fromCol].getDoubleVal();
         }
         else{
             continue;
         }
-
     }
     val=val/(toRow-fromRow+1);
-
     return val;
-}
+}// Cell객체 생성을 하지 않고 바로 비교 후 값을 대입하여 오버헤드를 줄임.
 
 Excel::Excel(std::vector<std::vector<Cell>> data) {
     this->data=data;
