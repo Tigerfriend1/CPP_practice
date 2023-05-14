a7e7b5893469fd5a3dddee354199fc485530f11e
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-05-14 21:21:17 +0900

    custom vector impl

--- template_class/main.cpp
+++ template_class/main.cpp
@@ -4,7 +4,8 @@
 int main() {
     using namespace std;
     my_vector<int> vec = {1, 2, 3, 4, 5};
-    my_vector<int> vec2(vec); vec2[4] = 10;
+    my_vector<int> vec2(vec);
+    vec2[4] = 10;
     for (int i=0; i<vec2.size(); ++i)
         cout << vec2[i] << ' ' ;
     cout << '\n';
--- template_class/my_vector.h
+++ template_class/my_vector.h
@@ -12,15 +12,24 @@
 template<typename T>
 class my_vector {
 public:
-    explicit my_vector(size_t N=0) noexcept ;  // 생성자를 구현하시오
+    explicit my_vector(size_t N=0) noexcept : _data(new T[N]), _size(N) {}// 생성자를 구현하시오
 
-    my_vector(std::initializer_list<int> lst) noexcept ;  // 생성자를 구현하시오
+    my_vector(std::initializer_list<int> lst) noexcept : _data(new T[lst.size()]), _size(lst.size()){
+        std::copy(lst.begin(), lst.end(), _data.get());
+    }  // 생성자를 구현하시오
 
-    my_vector(const my_vector<T>& other) noexcept ; // 복사 생성자를 구현하시오 (deep copy)
+    my_vector(const my_vector<T>& other) noexcept : _data(new T[other.size()]), _size(other.size()) {
+        std::copy(other._data.get(), other._data.get() + other.size(), _data.get());
+    } // 복사 생성자를 구현하시오 (deep copy)
 
-    T& operator[](int i) ; // subscript operator, 오퍼레이터 오버로딩 하시오
+    T& operator[](int i) {
+        return _data[i];
+    } // subscript operator, 오퍼레이터 오버로딩 하시오
 
-    size_t size() const // 멤버 함수를 구현하시오
+    size_t size() const{
+        return _size;
+    } // 멤버 함수를 구현하시오
+
 
 private:
     size_t _size;
