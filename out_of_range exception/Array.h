0f8241e4125985109f7c3a27b699e1ecf4605408
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-06-18 17:56:58 +0900

    upload : try-catch practice

---
+++ out_of_range exception/Array.h
@@ -0,0 +1,25 @@
+//
+// Created by 김태경 on 2023/06/18.
+//
+
+#ifndef OUT_OF_RANGE_EXCEPTION_ARRAY_H
+#define OUT_OF_RANGE_EXCEPTION_ARRAY_H
+#include <cstddef>
+#include <exception>
+#include <string>
+
+template<typename T>
+class Array {
+public:
+    Array(size_t size) : size(size), data(new T[size]) {}
+    T& at(size_t index){
+        if (0<= index && index < size)
+            return data[index];
+        throw std::out_of_range(std::to_string(index));
+    }
+    ~Array() {delete[] data;}
+private:
+    T* data;
+    size_t size;
+};
+#endif //OUT_OF_RANGE_EXCEPTION_ARRAY_H
